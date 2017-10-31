a43b43bd5ff1916dd9c31c7e8db09e0c24e16c37
alexwalz <alexwalz@icloud.com>
2017-10-31 09:54:39 -0600

    final touches on "GEAR"

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     $("#gamertagSearch").hide()
     $("#searchButton").hide()
     $("#section-2").hide()
+    $("#section-landing").hide()
     setTimeout(function() {
         $(".d2-logo").animate({ 'opacity': '1' }, 3700);
     }, 1000);
@@ -47,6 +48,28 @@ $(".console-images").on("click", function() {
 
 var hashTagActive = "";
 $("#section-1-button").on("click touchstart", function(event) {
+    $("#section-landing").show()
+    if (hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+        event.preventDefault();
+        //calculate destination place
+        var dest = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+            dest = $(document).height() - $(window).height();
+        } else {
+            dest = $(this.hash).offset().top;
+        }
+        //go to destination
+        $('html,body').animate({
+            scrollTop: dest
+        }, 600, 'swing');
+        hashTagActive = this.hash;
+    }
+
+
+});
+
+var hashTagActive = "";
+$("#gearButton").on("click touchstart", function(event) {
     $("#section-2").show()
     if (hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
         event.preventDefault();
