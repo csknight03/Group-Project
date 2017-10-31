5c71505597ac34fd2c646ea068b14231710bb63f
alexwalz <alexwalz@icloud.com>
2017-10-31 16:30:19 -0600

    minor bug fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -110,9 +110,8 @@ $("#gearButton").on("click touchstart", function(event) {
 
 
 var hashTagActive = "";
-$("#lfgButton").on("click touchstart", function(event) {
+$("#lfgButton").on("click touchstart", function() {
     if (hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
-        event.preventDefault();
         //calculate destination place
         var dest = 0;
         if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
