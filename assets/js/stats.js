4782c57e343f5738ce5cb0136709cd79c3664900
Chase Adams <chaseme37@MacBook-Pro.local>
2017-11-04 12:09:26 -0600

    stats logic

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -9,6 +9,10 @@ var character3;
 var introMusic = new Audio("assets/audio/intro.mp3")
 introMusic.volume = 0.09;
 
+
+
+
+
 $(document).ready(function() {
     introMusic.play()
     $("#gamertagSearch").hide()
@@ -21,14 +25,8 @@ $(document).ready(function() {
     setTimeout(function() {
         $(".scroll").animate({ 'opacity': '1' }, 1000);
     }, 4500);
-    
-    
+        $(".diamond-shape2").addClass("pulse");   
 });
-
-
-// $("#section-1-button").on("click", function(){
-// $("#section-2").show()
-// })
 
 
 
@@ -39,82 +37,6 @@ $(".console-images").on("click", function(event) {
     $("#searchButton").show()
     $("#gamertagSearch").show()
 })
-
-var hashTagActive = "";
-$("#section-1-button").on("click touchstart", function(event) {
-    $("#section-landing").show()
-    if (hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
-        event.preventDefault();
-        //calculate destination place
-        var dest = 0;
-        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-            dest = $(document).height() - $(window).height();
-        } else {
-            dest = $(this.hash).offset().top;
-        }
-        //go to destination
-        $('html,body').animate({
-            scrollTop: dest
-        }, 600, 'swing');
-        hashTagActive = this.hash;
-    }
-
-    setTimeout(function() {
-        $("#button1").animate({ 'opacity': '1' }, 1700);
-    }, 1000);
-
-    setTimeout(function() {
-        $("#button2").animate({ 'opacity': '1' }, 1700);
-    }, 1400);
-
-    setTimeout(function() {
-        $("#button3").animate({ 'opacity': '1' }, 1700);
-    }, 1800);
-
-
-});
-
-var hashTagActive = "";
-$("#gearButton").on("click touchstart", function(event) {
-    $("#section-2").show()
-    if (hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
-        event.preventDefault();
-        //calculate destination place
-        var dest = 0;
-        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-            dest = $(document).height() - $(window).height();
-        } else {
-            dest = $(this.hash).offset().top;
-        }
-        //go to destination
-        $('html,body').animate({
-            scrollTop: dest
-        }, 600, 'swing');
-        hashTagActive = this.hash;
-    }
-
-
-});
-
-
-var hashTagActive = "";
-$("#lfgButton").on("click touchstart", function() {
-    if (hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
-        //calculate destination place
-        var dest = 0;
-        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-            dest = $(document).height() - $(window).height();
-        } else {
-            dest = $(this.hash).offset().top;
-        }
-        //go to destination
-        $('html,body').animate({
-            scrollTop: dest
-        }, 600, 'swing');
-        hashTagActive = this.hash;
-    }
-
-});
 
 
 $("#searchButton").on("click", function() {
@@ -132,10 +54,10 @@ function characterFind() {
     $("#characters").empty()
 
     var settings = {
-        "crossDomain": true,
-        "url": "https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/" + userPlatform + "/" + gamertag,
-        "method": "GET",
-        "headers": {
+        crossDomain: true,
+        url: "https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/" + userPlatform + "/" + gamertag,
+        method: "GET",
+        headers: {
             "x-api-key": apiToken,
         }
     }
@@ -145,10 +67,10 @@ function characterFind() {
                 var membershipid = response.Response[0].membershipId
 
                 var characterIds = {
-                    "crossDomain": true,
-                    "url": "https://www.bungie.net/Platform/Destiny2/" + userPlatform + "/Profile/" + membershipid + "/?components=Characters,205",
-                    "method": "GET",
-                    "headers": {
+                    crossDomain: true,
+                    url: "https://www.bungie.net/Platform/Destiny2/" + userPlatform + "/Profile/" + membershipid + "/?components=Characters,205",
+                    method: "GET",
+                    headers: {
                         "x-api-key": apiToken,
                     }
                 }
@@ -157,13 +79,13 @@ function characterFind() {
                                 $.ajax(characterIds).done(function(response) {
 
                                     $.each(response.Response.characters.data, function(key, value) {
-                                        var emblemBackground = value.emblemBackgroundPath
-                                        var dateLastPlayed = value.dateLastPlayed
-                                        var dateLastPlayed = dateLastPlayed.slice(0, -10);
-                                        var light = value.light;
-                                        var classType = value.classType
-                                        var minutesPlayed = value.minutesPlayedTotal
-                                        var characterID = key
+                                       var emblemBackground = value.emblemBackgroundPath
+                                       var dateLastPlayed = value.dateLastPlayed
+                                       var dateLastPlayed = dateLastPlayed.slice(0, -10);
+                                       var light = value.light;
+                                       var classType = value.classType
+                                       var minutesPlayed = value.minutesPlayedTotal
+                                       var characterID = key
                                         
 
                                     
@@ -179,15 +101,25 @@ function characterFind() {
                                             gamertag = gamertag.replace("%20", " ")
                                         }
                                         
-                                        var newCard = "<div class='col-sm-4'><div class='card destiny-card' style='width: 20rem;'><img class='card-img-top emblemBackground' src='https://bungie.net" + emblemBackground + "'alt='Card image cap'><div class='card-body'><h4 class='card-title gamertag-title'>" + gamertag + "</h4> <div class='destiny-card-content' id='card-content'> <p class='card-text classType text-center class-icon-" + characterID + "'>" + classType + "</p> <p class='light-symbol text-center'>✦ <span class='lightLevel'>" + light + "</span></p><br><p class='text-center time-played'>Time Played: " + minutesPlayed + " minutes</p><div class='row gun-icons-" + characterID + "'></div><div class='row gear-icons-" + characterID + "'></div></div></div>"
-                                        $("#characters").append(newCard)
+                                        var newCard = "<div class='col-sm-4'><div class='card destiny-card' style='width: 20rem;'>"
+                                        + "<img class='card-img-top emblemBackground' src='https://bungie.net" + emblemBackground + "'alt='Card image cap'>"
+                                        + "<div class='card-body'><h4 class='card-title gamertag-title'>" + gamertag + "</h4>"
+                                        + "<div class='destiny-card-content' id='card-content'> <p class='card-text classType text-center class-icon-" + characterID + "'>" 
+                                        + classType + "</p> <p class='light-symbol text-center'>✦ <span class='lightLevel'>" + light 
+                                        + "</span></p><br><p class='text-center time-played'>Time Played: " + minutesPlayed + " minutes</p>"
+                                        + "<div class='stats-text' id='games-played-" + characterID + "'></div>"
+                                        + "<div class='stats-text' id='win-percentage-" + characterID + "'></div>"
+                                        + "</div></div></div></div>"
+                                        
+                                        $("#characters").append(newCard);
 
+                                                                // let charID = characterID;
                                         
                                                                 var characterStat = {
-                                                                    "crossDomain": true,
-                                                                    "url":  "https://www.bungie.net/Platform/Destiny2/"+userPlatform+"/Account/"+membershipid+"/Character/"+characterID+"/Stats/?modes=39",
-                                                                    "method": "GET",
-                                                                    "headers": {
+                                                                    crossDomain: true,
+                                                                    url:  "https://www.bungie.net/Platform/Destiny2/"+userPlatform+"/Account/"+membershipid+"/Character/"+characterID+"/Stats/?modes=39",
+                                                                    method: "GET",
+                                                                    headers: {
                                                                         "x-api-key": apiToken,
                                                                     }
                                                                 }
@@ -205,14 +137,19 @@ function characterFind() {
                                                                     var gamesPlayed = stats.activitiesEntered.basic.displayValue
                                                                     var gamesWon = stats.activitiesWon.basic.displayValue
                                                                     var winPercentage = gamesWon/gamesPlayed
-                                                                    var assists = stats.assists.basic.displayValue
-                                                                    var deathDistance = stats.averageDeathDistance.basic.displayValue
-                                                                    var killDistance = stats.averageKillDistance.basic.displayValue
-                                                                    var totalDeaths = stats.death.basic.displayValue
-                                                                    var totalKills = stats.kills.basic.displayValue
-                                                                    var efficiency = stats.efficiency.basic.displayValue
+                                                                    var wpRounded = winPercentage.toFixed(2);
+                                                                    // assists = stats.assists.basic.displayValue
+                                                                    // deathDistance = stats.averageDeathDistance.basic.displayValue
+                                                                    // killDistance = stats.averageKillDistance.basic.displayValue
+                                                                    // totalDeaths = stats.death.basic.displayValue
+                                                                    // totalKills = stats.kills.basic.displayValue
+                                                                    // efficiency = stats.efficiency.basic.displayValue
                                                                     
                                                                     console.log(gamesPlayed)
+                                                                    console.log(characterID)
+
+                                                                    $("#games-played-" + characterID).text("Games Played: " + gamesPlayed);
+                                                                    $("#win-percentage-" + characterID).text("Win Percentage: " + (wpRounded * 100) + "%");
 
 
                                                                 })
